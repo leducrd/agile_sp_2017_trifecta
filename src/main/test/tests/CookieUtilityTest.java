f54c53aa9986491b93d7cccca945be1f0a9da633
leducrd <leducrd@gmail.com>
2017-10-05 21:59:57 -0500

    Implement CookieUtility class and getCookieValue method

--- src/main/test/tests/CookieUtilityTest.java
+++ src/main/test/tests/CookieUtilityTest.java
@@ -3,6 +3,9 @@ package tests;
 import javax.servlet.http.Cookie;
 
 import org.junit.Test;
+
+import util.CookieUtility;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,9 +14,9 @@ public class CookieUtilityTest {
 	@Test
 	public void getCookieValueTest() {
 		
-		final Cookie cookie = new Cookie("testCookie", "testing@gmail.com");
+		final Cookie[] cookies = {new Cookie("testCookie", "testing@gmail.com"), new Cookie("testCookie2", "test2@gmail.com")};
 		
-		assertThat(CookieUtility.getCookieValue(), is("testing@gmail.com"));
+		assertThat(CookieUtility.getCookieValue(cookies, "testCookie"), is("testing@gmail.com"));
 	}
 
 }
