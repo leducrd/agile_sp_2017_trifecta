a094ca727668dfbf579efcbf0825d8e99acb576d
leducrd <leducrd@gmail.com>
2017-10-11 21:26:48 -0500

    Add unit test for Vehicle class

--- src/main/test/tests/VehicleTest.java
+++ src/main/test/tests/VehicleTest.java
@@ -1,5 +1,22 @@
 package tests;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
 public class VehicleTest {
+	
+	@Test
+	public void vehicleTest() {
+		Vehicle vehicle = new Vehicle("1", "Crysler", "Seabring", "1994", "Lava Red", "4", false);
+		
+		assertThat(vehicle.getUserID(), is("1"));
+		assertThat(vehicle.getMake(), is("Crysler"));
+		assertThat(vehicle.getModel(), is("Seabring"));
+		assertThat(vehicle.getYear(), is("1994"));
+		assertThat(vehicle.getColor(), is("Lava Red"));
+		assertThat(vehicle.getMaxSeats(), is("4"));
+		assertThat(vehicle.getCanSmoke(), is(false));
+	}
 
 }
