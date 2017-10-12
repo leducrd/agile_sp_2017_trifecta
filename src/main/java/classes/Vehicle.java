6e04949a719a3dace5bf087e3d5f99d215d0dc1a
leducrd <leducrd@gmail.com>
2017-10-12 12:23:34 -0500

    Made Vehicle class a JavaBean

--- src/main/java/classes/Vehicle.java
+++ src/main/java/classes/Vehicle.java
@@ -1,7 +1,11 @@
 package classes;
 
-public class Vehicle {
+import java.io.Serializable;
 
+public class Vehicle implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
 	private int userID;
 	private String make;
 	private String model;
