264395dbfaaea2da49a94c5788003453783cd758
leducrd <leducrd@gmail.com>
2017-10-11 21:32:08 -0500

    Add getters and setters for Vehicle class

--- src/main/java/classes/Vehicle.java
+++ src/main/java/classes/Vehicle.java
@@ -23,6 +23,62 @@ public class Vehicle {
 		this.maxSeats = maxSeats;
 		this.canSmoke = canSmoke;
 	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getMaxSeats() {
+		return maxSeats;
+	}
+
+	public void setMaxSeats(int maxSeats) {
+		this.maxSeats = maxSeats;
+	}
+
+	public boolean isCanSmoke() {
+		return canSmoke;
+	}
+
+	public void setCanSmoke(boolean canSmoke) {
+		this.canSmoke = canSmoke;
+	}
 	
 	
 }
