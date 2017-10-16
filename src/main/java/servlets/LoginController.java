37d0bfe3e3ad49fc3831796af2a58aa120036dbe
leducrd <leducrd@gmail.com>
2017-10-15 21:26:18 -0500

    Clean up to remove unnecessary imports from files

--- src/main/java/servlets/LoginController.java
+++ src/main/java/servlets/LoginController.java
@@ -1,8 +1,6 @@
 package servlets;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,13 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import classes.AuthenticatedUser;
-import classes.Person;
 import dao.AuthenticatedUserDao;
-import dao.PersonDao;
 import impl.AuthenticatedUserDaoImpl;
 import impl.AuthenticatedUserException;
-import impl.PersonDaoException;
-import impl.PersonDaoImpl;
 
 /**
  * Servlet implementation class LoginController
@@ -39,16 +33,6 @@ public class LoginController extends HttpServlet {
 		try {
 			final String userName = request.getParameter("loginEmail");
 			final String password = request.getParameter("password");
-//			final PersonDao personDao = new PersonDaoImpl();
-//			final List<Person> people = personDao.retrievePeople();
-//			Person loggedInUser;
-//
-//			final List<Person> exactPerson = people
-//													.stream()
-//													.filter((person) -> person.getEmail().equals(userName))
-//													.filter((person) -> person.getPassword().equals(password))
-//													.collect(Collectors.toList());
-//			loggedInUser = exactPerson.get(0);
 			
 			AuthenticatedUser loggedInUser = authUser.retrieveUser(userName, password);
 			
